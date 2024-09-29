8f39bb23d1c1e6f88d9459fd7194a56954dbe1f0
Florian Schaessens <f.schaessens@gmail.com>
2024-09-29 16:52:48 +0200

    add Login animation and background

---
+++ client/src/components/Animations.jsx
@@ -0,0 +1,18 @@
+import "../styles/Animations.css"
+
+function Animations () {
+return (
+    <>
+    <span className="shootingStar s1"></span>
+    <span className="shootingStar s2"></span>
+    <span className="shootingStar s3"></span>
+    <span className="shootingStar s4"></span>
+    <span className="shootingStar s5"></span>
+    <span className="shootingStar s6"></span>
+    <span className="shootingStar s7"></span>
+    <span className="shootingStar s8"></span>
+    </>
+)
+}
+
+export default Animations
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
